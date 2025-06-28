efd5b791295befb90d585b5b33389e9dd1a038c3
Gideon <gideonkimaiyo254@gmail.com>
2025-06-28 09:02:36 +0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const dodger = document.getElementById("dodger");
+
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
@@ -8,23 +9,11 @@ function moveDodgerLeft() {
   }
 }
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  }
-});
-
-
-
-
-
-
-
 function moveDodgerRight() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
 
-  if (left < 360) { 
+  if (left < 360) {
     dodger.style.left = `${left + 1}px`;
   }
 }
